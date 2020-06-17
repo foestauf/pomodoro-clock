7379b7da4b4402ff04cc6723c85473b4ab05493d
foestauf <46908845+foestauf@users.noreply.github.com>
2020-06-17 14:53:59 +0000

    Deploying to gh-pages from  @ 58a44dbea918b35fabbb07176a1443bc2d029314 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro-clock/precache-manifest.056607bf19076fa066f5304c499a495f.js"
+  "/Pomodoro-clock/precache-manifest.9b04a07b23e1183e4ff529cc1c8b5501.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro-clock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pomodoro-clock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
