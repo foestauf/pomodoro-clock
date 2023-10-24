e780d7fcd758472ca240d2fc1aadfee7fdd11c91
Rob McKee <46908845+foestauf@users.noreply.github.com>
2023-10-23 20:30:49 -0400

    update index for react 18 (#169)
    * Update react monorepo to v18
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import './assets/fonts/digital-7.ttf';
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./assets/fonts/digital-7.ttf";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+root.render(<App tab="home" />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
