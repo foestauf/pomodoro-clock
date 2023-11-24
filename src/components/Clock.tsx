d0bcd2e6a4bd81901ac76b8dc1de6934c6bf24f8
Rob McKee <46908845+foestauf@users.noreply.github.com>
2023-11-23 22:18:49 -0500

    Vite Migration (#173)
    * Vite Migration: WIP
    
    * re-add playwright
    
    * config vite dev server for CI
    
    * fix clockify seconds
    
    * Pointing app to old timer
    
    Identified issue in new logic. Will address later.

---
+++ src/components/Clock.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const Clock: React.FC = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <p>{currentTime.toLocaleTimeString()}</p>;
+};
+
+export default Clock;
