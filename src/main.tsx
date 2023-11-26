e1e5b6176699e7b36e765cea33feb07439a787cb
foestauf <46908845+foestauf@users.noreply.github.com>
2023-11-25 19:05:18 -0500

    fix for timer logic
    Restoring new timer logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Timer from "./Timer";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Timer />
+    <App />
   </React.StrictMode>
 );
