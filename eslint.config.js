10121e1b6c1267006ae235578c041e09afb51c81
Robert McKee <foestauf@gmail.com>
2024-12-09 23:32:38 -0500

    add react hooks eslint and up eslint to v9

--- eslint.config.js
+++ eslint.config.js
@@ -6,25 +6,29 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
+import hooksPlugin from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
+  {
+    ignores: ["**/*.config.js", "**/dist/**", "**/*.config.ts"],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
     plugins: {
       "react-compiler": reactCompiler,
+      // @ts-ignore
+      "react-hooks": hooksPlugin,
     },
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
       parser: tsParser,
       ecmaVersion: "latest",
       sourceType: "module",
     },
+    // @ts-ignore
     rules: {
       "react-compiler/react-compiler": "error",
+      ...hooksPlugin.configs.recommended.rules,
     },
   }
 );
